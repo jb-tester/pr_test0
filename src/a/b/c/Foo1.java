aefced509a4b8edd48d50ff63c926b4d749e534e
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-10-12 12:53:28 +0300

    initial adding - Foo1.java, .gitignore

---
+++ src/a/b/c/Foo1.java
@@ -0,0 +1,10 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 10/12/2018.
+ * Project: pr_test0
+ * *******************************
+ */
+public class Foo1 {
+}
