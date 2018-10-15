a579e9ad9dda4210c279448b92cf9088cf2a8119
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-10-15 16:14:22 +0300

    jbtester: add Foo4.java

---
+++ src/a/b/c/Foo4.java
@@ -0,0 +1,10 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 10/15/2018.
+ * Project: pr_test0_jbt
+ * *******************************
+ */
+public class Foo4 {
+}
